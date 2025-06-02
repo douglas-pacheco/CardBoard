d4486a6b418ac91c7d66026a908643a84978796d
Douglas Pacheco <douglas.pacheco@edu.pucrs.br>
2025-06-02 14:31:48 -0300

    first commit

---
+++ src/test/java/edu/dio/CardBoard/CardBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dio.CardBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
