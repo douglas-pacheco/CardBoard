8b7a2b161e74b91c5e2bd1af98f293dd06c9094b
Douglas Pacheco <douglas.pacheco@edu.pucrs.br>
2025-06-02 23:15:08 -0300

    fixing entities and allowing flyway to do his thing

--- src/main/java/edu/dio/CardBoard/persistence/entity/BoardColumnEntity.java
+++ src/main/java/edu/dio/CardBoard/persistence/entity/BoardColumnEntity.java
@@ -1,14 +1,19 @@
 package edu.dio.CardBoard.persistence.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 public class BoardColumnEntity {
 
@@ -19,12 +24,26 @@ public class BoardColumnEntity {
     private BoardColumnKindEnum kind;
 
     @ManyToOne
-    private BoardEntity board ;
+    private BoardEntity board;
+
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
-
     @OneToMany
     private List<CardEntity> cards;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        BoardColumnEntity that = (BoardColumnEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
 
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
